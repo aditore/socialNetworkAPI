d15fac3e4a74896ffc7d801f65e9677ad0bf99c8
aditore <anthonyditore@gmail.com>
2022-04-21 16:27:38 -0700

    adding reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,9 +1,36 @@
-const { Schema } = require('mongoose');
+const { Schema, Types } = require('mongoose');
 
 const reactionSchema = new Schema(
     {
-        reactionId: {}
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) {
+                    return date.toLocaleDateString();
+                }
+            }
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false
     }
-)
+);
 
 module.exports = reactionSchema
